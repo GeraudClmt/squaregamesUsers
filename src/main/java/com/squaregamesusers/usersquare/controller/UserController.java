78aef629aa1fb8335f17072495385444f3c2beab
GeraudClmt <geraud-yakou.clement@le-campus-numerique.fr>
2025-11-27 15:40:07 +0100

    End

--- src/main/java/com/squaregamesusers/usersquare/controller/UserController.java
+++ src/main/java/com/squaregamesusers/usersquare/controller/UserController.java
@@ -2,7 +2,7 @@ package com.squaregamesusers.usersquare.controller;
 
 import java.util.UUID;
 
-import org.springframework.http.HttpStatus;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -12,7 +12,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 
 import com.squaregamesusers.usersquare.dto.UserDto;
 import com.squaregamesusers.usersquare.request.CreateUserParamsRequest;
